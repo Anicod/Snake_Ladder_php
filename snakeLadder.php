5f4bad1590b7873c48405c0b751473a7182776be
Anicod <animeshraj735@gmail.com>
2022-09-09 03:54:34 +0530

    added: player strated at position 0

---
+++ snakeLadder.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Snake and ladder</title>
+</head>
+<body>
+    <?php
+    define($playerAPosition, 0);
+    $dice = rand(1, 6);
+    ?>
+</body>
+</html>
